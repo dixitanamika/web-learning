1e41efd9b2068909d5d96ed5c4d8e516d9da61f1
Anamika <dixitanamika206@gmail.com>
2023-11-30 11:38:48 +0530

    heap memory

--- js/test.js
+++ js/test.js
@@ -1,7 +1,16 @@
-//Stack (Primitive) Heap (Non-Primitive)
+//Stack (Primitive)
 let myName = "anamikadixit";
 let anotherName = myName;
 anotherName = "dixitanamika";
 
 console.log(myName);
 console.log(anotherName);
+
+//Heap (Non-Primitive)
+let userOne = {
+  email: "anamika@gmail.com",
+};
+let userTwo = userOne;
+userTwo.email = "dixit@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
